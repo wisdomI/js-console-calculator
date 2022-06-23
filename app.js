693b1cc0e14b7963350516bec3f273e911744294
wisdomI <81252474+wisdomI@users.noreply.github.com>
2022-06-23 16:38:28 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+let opr = prompt("operator: ");
+let num1 = parseInt(prompt("number 1: "));
+let num2 = parseInt(prompt("number 2: "));
+
+if (opr = "+") {
+    console.log(num1 + num2);    
+}   else if(opr = "-") 
+    console.log(num1 - num2);
+    else if (opr = "*") {
+        console.log(num1 * num2);
+    } 
+    else if ("/") {
+        console.log(num1 / num2);
+    }
+    else {
+    console.log("no input")
+}
+
+
+
